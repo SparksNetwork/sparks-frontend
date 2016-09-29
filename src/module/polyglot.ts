9c5669abfc917709a3215ceeadafa713be730555
Jeremy Wells <jemmyw@gmail.com>
2016-09-30 12:58:00 +1300

    fix(Translations): Default to en-US if no translations
    Fixes different users langauges not existing

--- src/module/polyglot.ts
+++ src/module/polyglot.ts
@@ -2,7 +2,8 @@ import { Module, VNode, VNodeData } from '@motorcycle/dom';
 
 import Polyglot = require('node-polyglot');
 
-const userLanguage = navigator.language || (navigator as any).userLanguage || 'en-US';
+const defaultLanguage = 'en-US';
+const userLanguage = navigator.language || (navigator as any).userLanguage || defaultLanguage;
 
 export interface PolyglotVNodeData extends VNodeData {
   polyglot: any;
@@ -14,7 +15,7 @@ export interface PolyglotVNode extends VNode {
 
 export function makePolyglotModule (translations: any): Module {
   const polyglot = new Polyglot();
-  polyglot.extend(translations[userLanguage]);
+  polyglot.extend(translations[userLanguage] || translations[defaultLanguage]);
 
   function internationlizeTextContent(_, vNode: PolyglotVNode) {
     const { phrase = null } = vNode.data && vNode.data.polyglot || {};
