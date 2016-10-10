3592bf7034652aa0e26c204dd887ef9fdd057347
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-10-10 19:20:03 -0400

    chore(typings): update typings installation

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/@most/create/index.d.ts" />
 /// <reference path="modules/history/index.d.ts" />
 /// <reference path="modules/node-polyglot/index.d.ts" />
 /// <reference path="modules/ramda/index.d.ts" />
