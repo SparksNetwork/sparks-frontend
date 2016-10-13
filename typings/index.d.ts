104bdfff29c71f5af360131dee5462cd8e9c590c
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-10-13 10:18:51 -0400

    chore(typings): rebuild typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,7 +1,3 @@
-/// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="modules/@most/create/index.d.ts" />
-/// <reference path="modules/history/index.d.ts" />
 /// <reference path="modules/node-polyglot/index.d.ts" />
 /// <reference path="modules/ramda/index.d.ts" />
 /// <reference path="modules/switch-path/index.d.ts" />
