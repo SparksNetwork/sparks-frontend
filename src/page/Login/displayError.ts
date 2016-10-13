5c606d9ad32013f4460e7f3cea251ac82aeaebe1
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-10-13 15:36:22 -0400

    style(Login): add missing semi-colon

--- src/page/Login/displayError.ts
+++ src/page/Login/displayError.ts
@@ -3,5 +3,5 @@ import { label } from '@motorcycle/dom';
 export function displayError(userExists: boolean | null) {
   return userExists === null || userExists
     ? null
-    : label({ style: { color: 'red' } }, 'User does not exist!')
+    : label({ style: { color: 'red' } }, 'User does not exist!');
 }
