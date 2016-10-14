8643cb1892cb4b3c95ee03a2aed6e6521a8f7681
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-10-14 15:18:13 -0400

    fix(displayError): use paragraph instead of label

--- src/pages/Login/displayError.ts
+++ src/pages/Login/displayError.ts
@@ -1,7 +1,7 @@
-import { label } from '@motorcycle/dom';
+import { p } from '@motorcycle/dom';
 
 export function displayError(userExists: boolean | null) {
   return userExists === null || userExists
     ? null
-    : label({ style: { color: 'red' } }, 'User does not exist!');
+    : p({ style: { color: 'red' } }, 'User does not exist!');
 }
