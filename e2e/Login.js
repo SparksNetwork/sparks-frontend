88161e6ab4446e7e970f83cb3196a94df0c800c0
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-10-16 03:13:24 -0400

    chore(e2e): test login page view

---
+++ e2e/Login.js
@@ -0,0 +1,55 @@
+const URL = 'http://localhost:8080/login'
+
+function containsText(element, text) {
+  return '//' + element + '[text()="' + text + '"]'
+}
+
+describe('Landing page', function () {
+  after((browser, done) => {
+    browser.end() // close browser
+    done() // end tests
+  })
+
+  it('should show login form', (browser) => {
+    browser
+      .url(URL)
+      .assert.visible('form')
+  })
+
+  describe('federated login buttons', () => {
+    it('should show google login button', (browser) => {
+      browser
+        .useXpath()
+        .assert.visible(containsText('*', 'Login with Google'))
+    })
+
+    it('should show facebook login button', (browser) => {
+      browser
+        .useXpath()
+        .assert.visible(containsText('*','Login with Facebook'))
+    })
+  })
+
+  describe('Email And Password Form', function () {
+    it('should show email label', (browser) => {
+      browser.useXpath()
+        .assert.visible(containsText('label', 'Email'))
+    })
+
+    it('should show password label', (browser) => {
+      browser.useXpath()
+        .assert.visible(containsText('label/*', 'Password'))
+    })
+
+    it('should show 2 inputs', function (browser, done) {
+      browser.elements('tag name', 'input', function (result) {
+        this.assert.equal(result.value.length, 2)
+      })
+    })
+
+    it('should show submit button', (browser) => {
+      browser.useXpath()
+        .assert.visible(containsText('*', 'Submit'))
+    })
+  })
+})
