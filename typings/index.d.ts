e4a5cc89bcd5606415b559bb78596fc18ffa172e
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-10-17 02:57:11 -0400

    chore(eslint): add eslint for e2e tests

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/sinon/index.d.ts" />
 /// <reference path="modules/history/index.d.ts" />
 /// <reference path="modules/node-polyglot/index.d.ts" />
 /// <reference path="modules/ramda/index.d.ts" />
