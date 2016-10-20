474da53b0f28f68b116a200f24efa45e24315f68
Frederik Krautwald <fkrautwald@gmail.com>
2016-10-20 04:39:36 +0200

    feat(domain/models): use domain models with User

--- src/domain/models/User.test.ts
+++ src/domain/models/User.test.ts
@@ -1,47 +1,69 @@
 /// <reference path="../../../typings/index.d.ts" />
 import * as assert from 'assert';
 import { User } from './User';
+import { WebUrl } from './WebUrl';
+import { EmailAddress } from './EmailAddress';
 
-describe('User', () => {
-  it('should be a function', () => {
-    assert(typeof User === 'function');
+describe(`domain/models/User`, () => {
+  it(`should be a function`, () => {
+    assert.strictEqual(typeof User, `function`);
   });
 
-  describe('should be constructed with', () => {
-    it('an unique ID', () => {
-      const user = createUser({ uid: 'a' });
-      assert(user.id() === 'a');
-      const user2 = createUser({ uid: 'b' });
-      assert(user2.id() === 'b');
-    });
+  it(`should set unique id, given a unique id`, () => {
+    assertUid(`00000000-0000-0000-0000-000000000000`);
+    assertUid(`11111111-1111-1111-1111-111111111111`);
+  });
 
-    it('a fullName', () => {
-      const user = createUser({ fullName: 'fullName' });
-      assert(user.fullName() === 'fullName');
-      const user2 = createUser({ fullName: 'other' });
-      assert(user2.fullName() === 'other');
-    });
+  it(`should set display name, given a display name`, () => {
+    assertDisplayName(`display name`);
+    assertDisplayName(`other name`);
+  });
 
-    it('a portraitUrl', () => {
-      const user = createUser({ portraitUrl: 'url' });
-      assert(user.portraitUrl() === 'url');
-      const user2 = createUser({ portraitUrl: 'other' });
-      assert(user2.portraitUrl() === 'other');
-    });
+  it(`should set portrait URL, given a portrait URL`, () => {
+    assertPortraitUrl(new WebUrl(`http://www.example.com/image.jpg`));
+    assertPortraitUrl(new WebUrl(`http://www.other.com/image.jpg`));
+  });
 
-    it('an email', () => {
-      const user = createUser({ email: 'user@sparks.network' });
-      assert(user.email() === 'user@sparks.network');
-      const user2 = createUser({ email: 'other@sparks.network' });
-      assert(user2.email() === 'other@sparks.network');
-    });
+  it(`should set email address, given an email address`, () => {
+    assertEmailAddress(new EmailAddress(`mailbox@example.com`));
+    assertEmailAddress(new EmailAddress(`other@example.com`));
   });
 });
 
-function createUser(options) {
-  const uid = options.uid || '';
-  const fullName = options.fullName || '';
-  const portraitUrl = options.portraitUrl || '';
-  const email = options.email || '';
-  return new User(uid, fullName, portraitUrl, email);
+function sutFixture(
+  uid: string,
+  displayName: string = ``,
+  portraitUrl: WebUrl = new WebUrl(`http://www.example.com/image.jpg`),
+  emailAddress: EmailAddress = new EmailAddress(`mailbox@example.com`)
+): User {
+  return new User(uid, displayName, portraitUrl, emailAddress);
+}
+
+function assertUid(uid: string) {
+  const sut = sutFixture(uid);
+  assert.strictEqual(sut.uid(), uid);
+}
+
+function assertDisplayName(displayName: string) {
+  const sut = sutFixture(`00000000-0000-0000-0000-000000000000`, displayName);
+  assert.strictEqual(sut.displayName(), displayName);
+}
+
+function assertPortraitUrl(portraitUrl: WebUrl) {
+  const sut = sutFixture(
+    `00000000-0000-0000-0000-000000000000`,
+    `display name`,
+    portraitUrl
+  );
+  assert.deepStrictEqual(sut.portraitUrl(), portraitUrl);
+}
+
+function assertEmailAddress(emailAddress: EmailAddress) {
+  const sut = sutFixture(
+    `00000000-0000-0000-0000-000000000000`,
+    `display name`,
+    new WebUrl(`http://www.example.com/image.jpg`),
+    emailAddress
+  );
+  assert.deepStrictEqual(sut.emailAddress(), emailAddress);
 }
--- src/domain/models/User.ts
+++ src/domain/models/User.ts
@@ -1,21 +1,57 @@
-// @TODO uid, fullName, portraitUrl, and email should be value objects 
-export class User {
-  constructor(private _uid: string, private _fullName: string,
-              private _portraitUrl: string, private _email: string) {}
+import { WebUrl } from './WebUrl';
+import { EmailAddress } from './EmailAddress';
 
-  id() {
+// @TODO uid and email should be value objects
+export class User {
+  private _uid: string;
+
+  private _displayName: string;
+
+  private _portraitUrl: WebUrl;
+
+  private _emailAddress: EmailAddress;
+
+  constructor(
+    uid: string,
+    displayName: string,
+    portraitUrl: WebUrl,
+    emailAddress: EmailAddress
+  ) {
+    this.setUid(uid);
+    this.setDisplayName(displayName);
+    this.setPortraitUrl(portraitUrl);
+    this.setEmailAddress(emailAddress);
+  }
+
+  uid(): string {
     return this._uid;
   }
 
-  fullName() {
-    return this._fullName;
+  displayName(): string {
+    return this._displayName;
   }
 
-  portraitUrl() {
+  portraitUrl(): WebUrl {
     return this._portraitUrl;
   }
 
-  email() {
-    return this._email;
+  emailAddress(): EmailAddress {
+    return this._emailAddress;
+  }
+
+  private setUid(uid: string) {
+    this._uid = uid;
+  }
+
+  private setDisplayName(displayName: string) {
+    this._displayName = displayName;
+  }
+
+  private setPortraitUrl(portraitUrl: WebUrl) {
+    this._portraitUrl = portraitUrl;
+  }
+
+  private setEmailAddress(emailAddress: EmailAddress) {
+    this._emailAddress = emailAddress;
   }
 }
