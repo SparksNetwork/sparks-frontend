abd542bb97ea4dbd1066ced181e3752f22ef912f
Frederik Krautwald <fkrautwald@gmail.com>
2016-11-02 12:58:34 +0100

    chore(typings): update typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="globals/core-js/index.d.ts" />
+/// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="modules/history/index.d.ts" />
