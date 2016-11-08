78bdc1263179d227d0cfc94873c9248030aabe4a
Frederik Krautwald <fkrautwald@gmail.com>
2016-11-08 09:49:47 +0100

    style(ui/components): format UserRegistration test

--- src/ui/components/UserRegistration/index.test.ts
+++ src/ui/components/UserRegistration/index.test.ts
@@ -2,8 +2,8 @@
 import * as assert from 'assert';
 import { VNode, DOMSource, mockDOMSource } from '@motorcycle/dom';
 import {
-    UserRegistration, UserRegistrationSinks, UserRegistrationSources,
-    UserRegistrationModel
+  UserRegistration, UserRegistrationSinks, UserRegistrationSources,
+  UserRegistrationModel
 } from './';
 import * as styles from './styles';
 const domSelect = require('snabbdom-selector').default;
@@ -11,118 +11,105 @@ const domSelect = require('snabbdom-selector').default;
 // This function exists because @motorcycle/dom v3.0.0 does not yet
 // have a common interface for mockDOMSource to implement
 function mockAsDomSource(mockConfig): DOMSource {
-    return mockDOMSource(mockConfig) as any as DOMSource;
+  return mockDOMSource(mockConfig) as any as DOMSource;
 }
 
 let defaultSources: UserRegistrationSources = {
-    DOM: mockAsDomSource({})
+  DOM: mockAsDomSource({})
 };
 
 describe(`UserRegistration component`, () => {
-    it(`has a DOM stream in its sinks`, () => {
-        const sinks: UserRegistrationSinks = UserRegistration(defaultSources);
-        Function.prototype(sinks);
+  it(`has a DOM stream in its sinks`, () => {
+    const sinks: UserRegistrationSinks = UserRegistration(defaultSources);
+    Function.prototype(sinks);
+  });
+
+  describe(`view`, () => {
+    it(`has a styled DIV element as root`, (done) => {
+      const sinks: UserRegistrationSinks = UserRegistration(defaultSources);
+
+      sinks.DOM.observe((view: VNode) => {
+        const matches = domSelect(`div.${styles.uniqueRoot}`, view);
+
+        assert.strictEqual(matches.length, 1);
+      })
+        .catch(done);
+
+      done();
     });
 
-    describe(`view`, () => {
-        it(`has a styled DIV element as root`, (done) => {
-            const sinks: UserRegistrationSinks = UserRegistration(defaultSources);
+    it(`has an email address Input component`, (done) => {
+      const sinks: UserRegistrationSinks = UserRegistration(defaultSources);
 
-            sinks.DOM.observe((view: VNode) => {
-                const matches = domSelect(`div.${styles.uniqueRoot}`, view);
+      sinks.DOM.observe((view: VNode) => {
+        const matches = domSelect(`.sn-input #UserRegistrationEmailAddressInput`, view);
 
-                assert.strictEqual(matches.length, 1);
-            })
-                .catch(done);
+        assert.strictEqual(matches.length, 1);
+        assert.strictEqual(matches[0].data.props.type, `email`);
+      })
+        .catch(done);
 
-            done();
-        });
-
-        it(`has a FORM element`, (done) => {
-            const sinks: UserRegistrationSinks = UserRegistration(defaultSources);
-
-            sinks.DOM.observe((view: VNode) => {
-                const matches = domSelect(`div.${styles.uniqueRoot} form`, view);
-
-                assert.strictEqual(matches.length, 1);
-            })
-                .catch(done);
-
-            done();
-        });
-
-        it(`has an email address Input component`, (done) => {
-            const sinks: UserRegistrationSinks = UserRegistration(defaultSources);
-
-            sinks.DOM.observe((view: VNode) => {
-                const matches = domSelect(`.sn-input #UserRegistrationEmailAddressInput`, view);
-
-                assert.strictEqual(matches.length, 1);
-                assert.strictEqual(matches[0].data.props.type, `email`);
-            })
-                .catch(done);
-
-            done();
-        });
-
-        it(`has a password Input component`, (done) => {
-            const sinks: UserRegistrationSinks = UserRegistration(defaultSources);
-
-            sinks.DOM.observe((view: VNode) => {
-                const matches = domSelect(`.sn-input #UserRegistrationPasswordInput`, view);
-
-                assert.strictEqual(matches.length, 1);
-                assert.strictEqual(matches[0].data.props.type, `password`);
-            })
-                .catch(done);
-
-            done();
-        });
-
-        it(`has a PasswordStrength component`, (done) => {
-            const sinks: UserRegistrationSinks = UserRegistration(defaultSources);
-
-            sinks.DOM.observe((view: VNode) => {
-                const matches = domSelect(`.sn-password-strength`, view);
-
-                assert.strictEqual(matches.length, 1);
-            })
-                .catch(done);
-
-            done();
-        });
-
-        it(`has a sign-up Button component`, (done) => {
-            const sinks: UserRegistrationSinks = UserRegistration(defaultSources);
-
-            sinks.DOM.observe((view: VNode) => {
-                const matches = domSelect(`.sn-button#UserRegistrationSignUpButton`, view);
-
-                assert.strictEqual(matches.length, 1);
-                assert.strictEqual(matches[0].children[0].text, `Sign up`);
-            })
-                .catch(done);
-
-            done();
-        });
+      done();
     });
 
-    it(`has a model stream in its sinks`, () => {
-        const sinks: UserRegistrationSinks = UserRegistration(defaultSources);
+    it(`has a password Input component`, (done) => {
+      const sinks: UserRegistrationSinks = UserRegistration(defaultSources);
 
-        assert.ok(sinks.hasOwnProperty(`model$`));
+      sinks.DOM.observe((view: VNode) => {
+        const matches = domSelect(`.sn-input #UserRegistrationPasswordInput`, view);
+
+        assert.strictEqual(matches.length, 1);
+        assert.strictEqual(matches[0].data.props.type, `password`);
+      })
+        .catch(done);
+
+      done();
     });
 
-    describe(`model stream`, () => {
-        it(`emits model objects`, (done) => {
-            const sinks: UserRegistrationSinks = UserRegistration(defaultSources);
+    it(`has a PasswordStrength component`, (done) => {
+      const sinks: UserRegistrationSinks = UserRegistration(defaultSources);
 
-            sinks.model$.observe((model: UserRegistrationModel) => {
-                assert.strictEqual(typeof model, `object`);
-            })
-                .catch(done);
+      sinks.DOM.observe((view: VNode) => {
+        const matches = domSelect(`.sn-password-strength`, view);
 
-            done();
-        });
+        assert.strictEqual(matches.length, 1);
+      })
+        .catch(done);
+
+      done();
     });
+
+    it(`has a sign-up Button component`, (done) => {
+      const sinks: UserRegistrationSinks = UserRegistration(defaultSources);
+
+      sinks.DOM.observe((view: VNode) => {
+        const matches = domSelect(`.sn-button#UserRegistrationSignUpButton`, view);
+
+        assert.strictEqual(matches.length, 1);
+        assert.strictEqual(matches[0].children[0].text, `Sign up`);
+      })
+        .catch(done);
+
+      done();
+    });
+  });
+
+  it(`has a model stream in its sinks`, () => {
+    const sinks: UserRegistrationSinks = UserRegistration(defaultSources);
+
+    assert.ok(sinks.hasOwnProperty(`model$`));
+  });
+
+  describe(`model stream`, () => {
+    it(`emits model objects`, (done) => {
+      const sinks: UserRegistrationSinks = UserRegistration(defaultSources);
+
+      sinks.model$.observe((model: UserRegistrationModel) => {
+        assert.strictEqual(typeof model, `object`);
+      })
+        .catch(done);
+
+      done();
+    });
+  });
 });
