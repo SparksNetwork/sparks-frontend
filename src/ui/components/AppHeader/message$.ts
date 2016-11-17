d04167d38cc16044300812cad95c54a340ef9069
Frederik Krautwald <fkrautwald@gmail.com>
2016-11-17 04:36:42 +0100

    refactor(ui/components): extract logic from `message$` to `update`

--- src/ui/components/AppHeader/message$.ts
+++ src/ui/components/AppHeader/message$.ts
@@ -5,13 +5,6 @@ import { combineObj } from '../../../helpers';
 
 export type Message =
   {
-    top: number;
-    transitionDuration: string;
-    hasShadow: boolean;
-  };
-
-type ScrollProps =
-  {
     height: number;
     scrollTop: number;
   };
@@ -40,105 +33,12 @@ export function message$(sources: AppHeaderSources): Stream<Message> {
       .startWith(0);
 
   const message$: Stream<Message> =
-    combineObj<ScrollProps>(
+    combineObj<Message>(
       {
         height$,
         scrollTop$,
       }
-    )
-      .map(scrollState);
+    );
 
   return message$;
-}
-
-let _top: number = 0;
-let _previousScrollTop: number = 0;
-let _initScrollTop: number = 0;
-let _initTimestamp: number = 0;
-let _wasScrollingDown: boolean = false;
-let _previousTimestamp: number = 0;
-
-function scrollState(props: ScrollProps): Message {
-  const {
-    height,
-    scrollTop,
-  } = props;
-
-  let top: number = 0;
-  let transitionDuration = `0ms`;
-  let hasShadow: boolean = false;
-
-  if (!height)
-    return { top, transitionDuration, hasShadow };
-
-  const maxAllowedMoveDistance: number = height + 5;
-  const previousTop: number = _top;
-  const deltaScrollTop: number = scrollTop - _previousScrollTop;
-
-  top = clamp(previousTop + deltaScrollTop, 0, maxAllowedMoveDistance);
-
-  const absoluteDeltaScrollTop: number = Math.abs(deltaScrollTop);
-  const now: number = Date.now();
-  const isScrollingDown: boolean = scrollTop > _previousScrollTop;
-
-  const isNormalScrollSpeed: boolean =
-    absoluteDeltaScrollTop < 100;
-
-  const shouldInitScrollPosition: boolean =
-    isNormalScrollSpeed &&
-    (now - _initTimestamp > 300 || _wasScrollingDown !== isScrollingDown);
-
-  if (shouldInitScrollPosition) {
-    _initScrollTop = scrollTop;
-    _initTimestamp = now;
-  }
-
-  const scrolledBeyondAllowedMoveDistance: boolean =
-    isNormalScrollSpeed && scrollTop >= maxAllowedMoveDistance;
-
-  const allowHeaderToSnap: boolean =
-    isNormalScrollSpeed &&
-    scrolledBeyondAllowedMoveDistance &&
-    (Math.abs(_initScrollTop - scrollTop) > 30 ||
-      absoluteDeltaScrollTop > 10);
-
-  const snapOutOfScreen: boolean =
-    isNormalScrollSpeed && allowHeaderToSnap && isScrollingDown;
-
-  if (snapOutOfScreen)
-    top = maxAllowedMoveDistance;
-  else if (allowHeaderToSnap)
-    top = 0;
-
-  if (allowHeaderToSnap) {
-    const scrollVelocity: number =
-      deltaScrollTop / (now - _previousTimestamp);
-
-    transitionDuration =
-      `${clamp((top - previousTop) / scrollVelocity, 0, 300)}ms`;
-
-  } else if (scrolledBeyondAllowedMoveDistance) {
-    top = previousTop;
-  }
-
-  _previousScrollTop = scrollTop;
-  _top = top;
-  _wasScrollingDown = isScrollingDown;
-  _previousTimestamp = now;
-
-  const isOnScreen: boolean =
-    height !== 0 && _top < height;
-
-  const hasContentBelow: boolean =
-    _top === 0 ?
-      scrollTop > 0 :
-      scrollTop - maxAllowedMoveDistance >= 0;
-
-  hasShadow = isOnScreen && hasContentBelow;
-
-  return { top, transitionDuration, hasShadow };
-}
-
-function clamp(value: number, min: number, max: number): number {
-  return Math.min(max, Math.max(min, value));
 }
--- src/ui/components/AppHeader/update.ts
+++ src/ui/components/AppHeader/update.ts
@@ -1,8 +1,100 @@
 import { Message } from './message$';
 import { AppHeaderModel } from './';
 
+let _top: number = 0;
+let _previousScrollTop: number = 0;
+let _initScrollTop: number = 0;
+let _initTimestamp: number = 0;
+let _wasScrollingDown: boolean = false;
+let _previousTimestamp: number = 0;
+
 export function update(message: Message): AppHeaderModel {
-  const { transitionDuration, top, hasShadow } = message;
+  const { height, scrollTop } = message;
+
+  let top: number = 0;
+  let transitionDuration: string = `0ms`;
+  let hasShadow: boolean = false;
+
+  if (!height)
+    return appHeaderModel({ transitionDuration, top, hasShadow });;
+
+  const maxAllowedMoveDistance: number = height + 5;
+  const previousTop: number = _top;
+  const deltaScrollTop: number = scrollTop - _previousScrollTop;
+
+  top = clamp(previousTop + deltaScrollTop, 0, maxAllowedMoveDistance);
+
+  const absoluteDeltaScrollTop: number = Math.abs(deltaScrollTop);
+
+  // @TODO Date must come through a driver.
+  const now: number = Date.now();
+  const isScrollingDown: boolean = scrollTop > _previousScrollTop;
+
+  const SCROLL_SPEED_LIMIT_PIXELS: number = 100;
+
+  const isNormalScrollSpeed: boolean =
+    absoluteDeltaScrollTop < SCROLL_SPEED_LIMIT_PIXELS;
+
+  const INIT_SCROLL_TIME_TO_WAIT: number = 300;
+
+  const shouldInitScrollPosition: boolean =
+    isNormalScrollSpeed &&
+    (now - _initTimestamp > INIT_SCROLL_TIME_TO_WAIT ||
+      _wasScrollingDown !== isScrollingDown);
+
+  if (shouldInitScrollPosition) {
+    _initScrollTop = scrollTop;
+    _initTimestamp = now;
+  }
+
+  const scrolledBeyondAllowedMoveDistance: boolean =
+    isNormalScrollSpeed && scrollTop >= maxAllowedMoveDistance;
+
+  const SCROLL_SNAP_SENSITIVITY_PIXELS: number = 10;
+  const PAUSED_SCROLL_SNAP_SENSITIVITY_PIXELS: number = 30;
+
+  const allowHeaderToSnap: boolean =
+    isNormalScrollSpeed &&
+    scrolledBeyondAllowedMoveDistance &&
+    (Math.abs(_initScrollTop - scrollTop) > PAUSED_SCROLL_SNAP_SENSITIVITY_PIXELS ||
+      absoluteDeltaScrollTop > SCROLL_SNAP_SENSITIVITY_PIXELS);
+
+  const snapHeaderOutsideViewport: boolean =
+    isNormalScrollSpeed && allowHeaderToSnap && isScrollingDown;
+
+  if (snapHeaderOutsideViewport)
+    top = maxAllowedMoveDistance;
+  else if (allowHeaderToSnap)
+    top = 0;
+
+  if (allowHeaderToSnap) {
+    const scrollVelocity: number =
+      deltaScrollTop / (now - _previousTimestamp);
+
+    const MAX_TRANSITION_DURATION: number = 300;
+    const MIN_TRANSITION_DURATION: number = 0;
+
+    transitionDuration =
+      clamp((top - previousTop) / scrollVelocity,
+        MIN_TRANSITION_DURATION,
+        MAX_TRANSITION_DURATION) +
+      `ms`;
+
+  } else if (scrolledBeyondAllowedMoveDistance) {
+    top = previousTop;
+  }
+
+  updateGlobals({ scrollTop, top, isScrollingDown, now });
+
+  hasShadow =
+    isOnScreen(height) &&
+    hasContentBelow(scrollTop, maxAllowedMoveDistance);
+
+  return appHeaderModel({ transitionDuration, top, hasShadow });;
+}
+
+function appHeaderModel(specs: any): AppHeaderModel {
+  const { transitionDuration, top, hasShadow } = specs;
 
   const model: AppHeaderModel =
     {
@@ -14,4 +106,27 @@ export function update(message: Message): AppHeaderModel {
     };
 
   return model;
+}
+
+function clamp(value: number, min: number, max: number): number {
+  return Math.min(max, Math.max(min, value));
+}
+
+function updateGlobals(specs: any) {
+  const {scrollTop, top, isScrollingDown, now} = specs;
+
+  _previousScrollTop = scrollTop;
+  _top = top;
+  _wasScrollingDown = isScrollingDown;
+  _previousTimestamp = now;
+}
+
+function isOnScreen(height): boolean {
+  return height !== 0 && _top < height;
+}
+
+function hasContentBelow(scrollTop, maxAllowedMoveDistance): boolean {
+  return _top === 0 ?
+    scrollTop > 0 :
+    scrollTop - maxAllowedMoveDistance >= 0;
 }
