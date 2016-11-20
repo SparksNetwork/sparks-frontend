a6e3b9df4b49e1681e507d1fbe757f2f1c1415af
sd <sdebaun74@gmail.com>
2016-11-20 02:03:52 -0800

    feat(c169): actually added signin route

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,15 +1,17 @@
-import { Stream, map } from 'most'
+import { Stream, map, merge } from 'most'
 import { Location, Pathname } from '@motorcycle/history'
-import { div, h2, a } from '@motorcycle/dom'
+import { div, h2, a, p } from '@motorcycle/dom'
 import { Routing, MainSources, MainSinks } from '../app'
 
 import { ConnectScreen } from './connect'
+import { SignInScreen } from './signin'
 
 export function main(sources: MainSources): MainSinks {
   const sinks$: Stream<MainSinks> =
     Routing({
       '/': Screen,
       '/connect': ConnectScreen,
+      '/signin': SignInScreen,
     }, sources)
 
   return {
@@ -19,20 +21,30 @@ export function main(sources: MainSources): MainSinks {
 }
 
 function Screen(sources: MainSources): MainSinks {
-  const router: Stream<Pathname> =
-    sources.dom.select('a').events('click')
+  const connect$: Stream<Pathname> =
+    sources.dom.select('a.connect').events('click')
     .tap(evt => evt.preventDefault())
     .map(() => '/connect')
 
+  const signin$: Stream<Pathname> =
+    sources.dom.select('a.signin').events('click')
+    .tap(evt => evt.preventDefault())
+    .map(() => '/signin')
+
   return {
     dom: map(view, sources.router.history()),
-    router,
+    router: merge(connect$, signin$),
   }
 }
 
 function view(location: Location) {
   return div([
       h2(`Home: ${location.pathname}`),
-      a({attrs: {href: '/connect'}}, 'Connect'),
+      div([
+        a('.connect', {attrs: {href: '/connect'}}, 'Connect'),
+        p(' | '),
+        a('.signin', {attrs: {href: '/signin'}}, 'Sign In'),
+      ]),
     ])
 }
+
