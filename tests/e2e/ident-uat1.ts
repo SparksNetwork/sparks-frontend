cc3b050b297b0cc2228a1f6614946c449a4bde0d
Frederik Krautwald <fkrautwald@gmail.com>
2016-12-14 23:16:28 +0100

    test(e2e): fix timeouts
    Remove arbitrary wait times and rely on defaults.
    Remove redundant `browser.end()`.

--- tests/e2e/ident-uat1.ts
+++ tests/e2e/ident-uat1.ts
@@ -4,17 +4,17 @@ export = {
   'IDENT UAT 1: Connect with Google': function (browser: NightWatchBrowser) {
     browser
       .url('localhost:8080/connect')
-      .waitForElementVisible('#page', 1000) // wait for the page to display
+      .waitForElementVisible('#page') // wait for the page to display
       .click('.c-btn-federated--google') // click the google button
-      .pause(2000) // give it time to redirect
+      .pause(5000) // give it time to redirect
       .assert.urlContains('ServiceLogin') // we are on the google page
-      .waitForElementPresent('#Email', 1000)
+      .waitForElementPresent('#Email')
       .setValue('#Email', process.env.GOOGLE_TEST_EMAIL)
       .click('#next')
-      .waitForElementPresent('#Passwd', 1000)
+      .waitForElementPresent('#Passwd')
       .setValue('#Passwd', process.env.GOOGLE_TEST_EMAIL_PASSWORD)
       .click('#signIn')
-      .waitForElementPresent('#user-email', 2000)
+      .waitForElementPresent('#user-email')
       .assert.containsText('#user-email', process.env.GOOGLE_TEST_EMAIL)
       .end();
   },
--- tests/e2e/ident-uat5.ts
+++ tests/e2e/ident-uat5.ts
@@ -4,20 +4,18 @@ export = {
   'IDENT UAT 5: Sign in with google': function (browser: NightWatchBrowser) {
     (browser.page as any).signin()
       .navigate()
-      .waitForElementVisible('#page', 1000) // wait for the page to display
+      .waitForElementVisible('#page') // wait for the page to display
       .click('.c-btn-federated--google')
       .api.pause(2000)
       .assert.urlContains('ServiceLogin') // we are on the google page
-      .waitForElementPresent('#Email', 1000)
+      .waitForElementPresent('#Email')
       .setValue('#Email', process.env.GOOGLE_TEST_EMAIL)
       .click('#next')
-      .waitForElementPresent('#Passwd', 1000)
+      .waitForElementPresent('#Passwd')
       .setValue('#Passwd', process.env.GOOGLE_TEST_EMAIL_PASSWORD)
       .click('#signIn')
-      .waitForElementPresent('#user-email', 2000)
+      .waitForElementPresent('#user-email')
       .assert.containsText('#user-email', process.env.GOOGLE_TEST_EMAIL)
       .end();
-
-    browser.end();
   },
 };
