ba2d17a5555b204c17f7fed8c50132d679eec295
Frederik Krautwald <fkrautwald@gmail.com>
2016-12-15 02:24:14 +0100

    test(e2e): add db user clean-up
    Delete connected Google user after end test.

--- tests/e2e/ident-uat-001-connect-with-google.ts
+++ tests/e2e/ident-uat-001-connect-with-google.ts
@@ -1,6 +1,35 @@
+import * as admin from 'firebase-admin';
+
 import { NightWatchBrowser } from 'nightwatch';
 
+const serviceAccount = require(process.env.FIREBASE_ADMINSDK_JSON);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
+
 export = {
+  afterEach: function (_: any, done: Function) {
+    (admin.auth() as any).getUserByEmail(process.env.GOOGLE_TEST_EMAIL)
+      .then((userRecord: any) => {
+        const uid: string = userRecord.uid;
+
+        (admin.auth() as any).deleteUser(uid)
+          .then(() => {
+            console.log(`Deleted process.env.GOOGLE_TEST_EMAIL from database`);
+            done();
+          })
+          .catch((error: Error) => {
+            throw error;
+          });
+      })
+      .catch((error: Error) => {
+        throw error;
+      });
+
+    done();
+  },
+
   'IDENT UAT 1: Connect with Google': function (browser: NightWatchBrowser) {
     browser
       .url('localhost:8080/connect')
