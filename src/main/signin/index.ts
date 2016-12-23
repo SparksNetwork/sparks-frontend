ad990282d029e6257f4845721b3cf31d09e0f956
brucou <bruno.couriol@gmail.com>
2016-12-23 20:50:03 +0100

    feat(ident) : UAT 8 - first draft

--- src/main/signin/index.ts
+++ src/main/signin/index.ts
@@ -55,7 +55,7 @@ function view() {
           li('.c-sign-in__list-item', [
             div('.c-textfield', [
               label([
-                input('.c-textfield__input', { props: { type: 'text', required: true } }),
+                input('.c-textfield__input.c-textfield__input--email', { props: { type: 'text', required: true } }),
                 span('.c-textfield__label', 'Email address'),
               ]),
             ]),
@@ -63,7 +63,7 @@ function view() {
           li('.c-sign-in__list-item', [
             div('.c-sign-in__password.c-textfield', [
               label([
-                input('.c-textfield__input', { props: { type: 'password', required: true } }),
+                input('.c-textfield__input.c-textfield__input--password', { props: { type: 'password', required: true } }),
                 span('.c-textfield__label', 'Password'),
               ]),
               a('.c-sign-in__password-forgot', { props: { href: '/forgot-password' } }, 'Forgot?'),
