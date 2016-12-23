a7d25df4e8c22f1f51a27fb3ca9a42c5f9247519
brucou <bruno.couriol@gmail.com>
2016-12-23 02:29:24 +0100

    feat(ident) : changed env variables

--- tests/e2e/ident-uat7.ts
+++ tests/e2e/ident-uat7.ts
@@ -1,14 +1,14 @@
 import {NightWatchBrowser} from 'nightwatch';
 
-const {EMAIL_AND_PASSWORD_TEST_EMAIL, EMAIL_AND_PASSWORD_TEST_PASSWORD} = process.env;
+const {EXISTING_EMAIL_USER_TEST_EMAIL, EXISTING_EMAIL_USER_TEST_PASSWORD} = process.env;
 
 export = {
   'IDENT UAT 7: Sign in with email from Connect': function (browser: NightWatchBrowser) {
     browser
       .url('localhost:8080/signin')
       .waitForElementVisible('#page', 1000) // wait for the page to display
-      .setValue('.c-textfield__input--email', EMAIL_AND_PASSWORD_TEST_EMAIL)
-      .setValue('.c-textfield__input--password', EMAIL_AND_PASSWORD_TEST_PASSWORD)
+      .setValue('.c-textfield__input--email', EXISTING_EMAIL_USER_TEST_EMAIL)
+      .setValue('.c-textfield__input--password', EXISTING_EMAIL_USER_TEST_PASSWORD)
       .click('.c-btn.c-btn--primary.c-sign-in__submit') // click submit button
       .pause(2000) // give it time to redirect
       .assert.urlContains('dash') // we are on the dashboard page
