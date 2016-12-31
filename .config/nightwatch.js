cb53ea044809ae7748ac23f1ab92df9d6e679dc3
brucou <bruno.couriol@gmail.com>
2016-12-31 17:19:23 +0100

    fix(config) : wip

---
+++ .config/nightwatch.js
@@ -0,0 +1,17 @@
+module.exports = (function (settings) {
+    if (process.platform === 'win32')
+        settings.selenium.cli_args['webdriver.chrome.driver'] =
+            './node_modules/.bin/chromedriver.cmd';
+    if (process.env.SELENIUM_HOST)
+        settings.selenium.host = process.env.SELENIUM_HOST;
+    if (process.env.SELENIUM_PORT)
+        settings.selenium.host = process.env.SELENIUM_PORT;
+    if (process.env.TRAVIS) {
+        var defaultSettings = settings.test_settings.default;
+        defaultSettings.desiredCapabilities.chromeOptions =
+            { args: ['--no-sandbox'] };
+        defaultSettings.globals.waitForConditionTimeout = 20000;
+    }
+    settings.page_objects_path = './tests/.tmp/page-objects';
+    return settings;
+})(process.env.LOCAL ? require('./nightwatch/local.ts') : require('./nightwatch/sauce.ts'));
