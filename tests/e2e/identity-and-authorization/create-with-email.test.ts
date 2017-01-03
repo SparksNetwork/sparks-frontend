97a1745752017f9702253f0c2d6a9362acc643f8
brucou <bruno.couriol@gmail.com>
2017-01-03 20:02:13 +0100

    fix(ident) : wip

--- tests/e2e/identity-and-authorization/create-with-email.test.ts
+++ tests/e2e/identity-and-authorization/create-with-email.test.ts
@@ -2,47 +2,10 @@ import { NightWatchBrowser } from 'nightwatch';
 import * as admin from 'firebase-admin';
 
 const {
-        FIREBASE_DATABASE_URL,
         EMAIL_AND_PASSWORD_TEST_EMAIL,
-        FIREBASE_ADMINSDK_TYPE,
-        FIREBASE_ADMINSDK_PROJECT_ID,
-        FIREBASE_ADMINSDK_PRIVATE_KEY_ID,
-        FIREBASE_ADMINSDK_PRIVATE_KEY,
-        FIREBASE_ADMINSDK_CLIENT_EMAIL,
-        FIREBASE_ADMINSDK_CLIENT_ID,
-        FIREBASE_ADMINSDK_AUTH_URI,
-        FIREBASE_ADMINSDK_TOKEN_URI,
-        FIREBASE_ADMINSDK_AUTH_PROVIDER_X509_CERT_URL,
-        FIREBASE_ADMINSDK_CLIENT_X509_CERT_URL
       } = process.env;
 
-function getAuthAdmin() {
-  try {
-    admin.app();
-  }
-  catch(e) {
-    const serviceAccount = {
-      "type": FIREBASE_ADMINSDK_TYPE,
-      "project_id": FIREBASE_ADMINSDK_PROJECT_ID,
-      "private_key_id": FIREBASE_ADMINSDK_PRIVATE_KEY_ID,
-      "private_key": FIREBASE_ADMINSDK_PRIVATE_KEY,
-      "client_email": FIREBASE_ADMINSDK_CLIENT_EMAIL,
-      "client_id": FIREBASE_ADMINSDK_CLIENT_ID,
-      "auth_uri": FIREBASE_ADMINSDK_AUTH_URI,
-      "token_uri": FIREBASE_ADMINSDK_TOKEN_URI,
-      "auth_provider_x509_cert_url": FIREBASE_ADMINSDK_AUTH_PROVIDER_X509_CERT_URL,
-      "client_x509_cert_url": FIREBASE_ADMINSDK_CLIENT_X509_CERT_URL
-    };
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      databaseURL: FIREBASE_DATABASE_URL,
-    });
-  }
-
-  return admin.auth() as any;
-}
-
-let authAdmin: any = getAuthAdmin();
+const authAdmin = admin.auth();
 
 function deleteUserAndReturnPromise(email: string) {
   return authAdmin.getUserByEmail(email)
