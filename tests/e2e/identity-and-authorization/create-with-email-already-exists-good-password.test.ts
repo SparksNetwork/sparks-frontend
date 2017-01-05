3dfe2e7e5650080f2e098e66b54c5ac898f5282a
brucou <bruno.couriol@gmail.com>
2017-01-05 08:50:43 +0100

    fix(ident): solving firefox failing test

--- tests/e2e/identity-and-authorization/create-with-email-already-exists-good-password.test.ts
+++ tests/e2e/identity-and-authorization/create-with-email-already-exists-good-password.test.ts
@@ -13,7 +13,7 @@ function execTest(browser: NightWatchBrowser) {
     .setValue('.c-textfield__input--email', EMAIL_AND_PASSWORD_TEST_EMAIL)
     .setValue('.c-textfield__input--password', EMAIL_AND_PASSWORD_TEST_PASSWORD)
     .click('.c-btn.c-btn--primary.c-sign-in__submit') // click submit button
-    .pause(4000) // give it time to redirect
+    .pause(6000) // give it time to redirect
     .assert.urlContains('dash') // we are on the dashboard page
     .end();
 }
