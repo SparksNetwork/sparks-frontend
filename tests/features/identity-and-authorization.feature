d882fc8e765f2682a321d3f81f37fd3a1f39cba9
brucou <bruno.couriol@gmail.com>
2017-01-06 21:47:32 +0100

    chore(config) : gherkin test file for UAT3 wip

--- tests/features/identity-and-authorization.feature
+++ tests/features/identity-and-authorization.feature
@@ -28,7 +28,6 @@ Scenario: Connect with Facebook
    Then I’m taken to my dashboard
     And I am signed in
 
-@wip @noci
 Scenario: Connect with email and password
   Given I’m not connected with "email and password"
    When I navigate to the "Connect" URL
---
+++ tests/features/step_definitions/index.ts
@@ -0,0 +1,10 @@
+import { switchToAFromB } from './switch-to-a-from-b.scenario';
+import { connectWithGoogleOrFacebook } from './connect-with-google-facebook.scenario';
+import {connectWithEmailAndPassword} from  './connect-with-email-and-password.scenario';
+
+export function test() {
+  switchToAFromB(this);
+  connectWithGoogleOrFacebook(this);
+  connectWithEmailAndPassword(this);
+  this.end();
+}
